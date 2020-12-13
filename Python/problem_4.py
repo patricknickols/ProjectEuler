8e33d5f064ca99420c70410cd515edea434962df
patricknickols <patricknickols@gmail.com>
2020-12-13 12:06:24 +0000

    Solved Problem 4
    (Note method is missing several obvious efficiency improvments)

--- Python/problem_4.py
+++ Python/problem_4.py
@@ -1,7 +1,14 @@
-
-
-
 def is_palindrome(number):
 	return (str(number) == str(number)[::-1])
 
-print is_palindrome(77)
+def largest_palindrome_product_n_digits(n):
+	largest = 0
+	for i in range(10**(n-1), 10**(n)):
+		for j in range(10**(n-1), 10**(n)):
+			product = i * j
+			if is_palindrome(product) and product > largest:
+				largest = product
+	return largest
+
+
+print(largest_palindrome_product_n_digits(3))
