7315950cd539506a1e9f03ce2dcaf39a072382b5
patricknickols <patricknickols@gmail.com>
2020-12-14 09:10:04 +0000

    Rewrote problem 2 to be more readable

--- Python/problem_2.py
+++ Python/problem_2.py
@@ -1,24 +1,21 @@
+
+#Supply n to compute F_n for n > 0
 def fibonacci(n):
-    answer = 2
-    ultimate = 1
+    f_n = 0                                #F_0
+    f_n_minus_one = 1                      #F_-1
     for i in range(n):
-        old_answer = answer
-        answer += ultimate
-        ultimate = old_answer
-    return ultimate
+        f_n_copy = f_n
+        f_n += f_n_minus_one
+        f_n_minus_one = f_n_copy
+    return f_n
 
-
-
+#Supply a bound upper_fib_bound to compute the sum of all even F_n such that F_n < upper_fib_bound
 def sum_of_even_fib(upper_fib_bound):
-    n = 1
+    n = 3                                   #First even term is F_3
     sum = 0
     while fibonacci(n) <= upper_fib_bound:
         sum += fibonacci(n)
-        n += 3
+        n += 3                              # Every third term is even
     return sum
 
-answer = sum_of_even_fib(4000000)
-print(fibonacci(1))
-print(fibonacci(4))
-print(fibonacci(7))
-print(answer)
+print(sum_of_even_fib(4000000))
