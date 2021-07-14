a3be2badb853829cf928d9a53f798e4ef7b4ba6b
patricknickols <patricknickols@gmail.com>
2021-07-14 13:28:10 +0100

    Tided problem 3, got problem 12 functioning

--- Python/problem_12.py
+++ Python/problem_12.py
@@ -1,13 +1,7 @@
 import math
 
-def list_constructor(n):
-    int_list = []
-    for i in range(n):
-        int_list.append(i)      #List is initialised with all values set to themselves
-    return int_list
-
 def primes(n):
-    prime_list = list_constructor(n)
+    prime_list = [i for i in range(n+1)]
     for i in range(2,n):
         if prime_list[i] != 0:   #Check that i has not been crossed off
             for j in range(2*i,n,i): #Cross off every number i away
@@ -15,12 +9,43 @@ def primes(n):
     prime_list[1] = 0
     return [prime for prime in prime_list if prime != 0]  #return list of uncrossed numbers
 
-
 def triangular_number(n):
-    return (n * (n+1))/2
+    return int((n*(n+1))/2)
 
-def prime_factorise_with_powers(n):
-    primes_and_powers = []
-    for prime in primes(n):
-        primes_and_powers[prime] = largest_power_less_than(prime, n)
+def prime_factors(big_num):
+    factors = []
+    for prime in primes(big_num // 2):  # cannot have a prime_factor larger than half of it, unless it is
+        if big_num % prime == 0:
+            factors.append(prime)
+    return factors
+
+def prime_factors_with_powers(n):
+    primes_and_powers = {factor: 0 for factor in prime_factors(n)}
+    for factor in primes_and_powers:
+        counter = 0
+        dividend = n
+        while dividend % factor == 0:
+            dividend = dividend / factor
+            counter += 1
+        primes_and_powers[factor] = counter
     return primes_and_powers
+
+def product(list):
+    result = 1
+    for element in list:
+        result *= element
+    return result
+
+def number_of_factors(n):
+    factors_powers = prime_factors_with_powers(n)
+    incr_exponents = [factors_powers[key] + 1 for key in factors_powers]
+    return product(incr_exponents)
+
+def first_tri_with_n_factors(n):
+    counter = 1200
+    while number_of_factors(triangular_number(counter)) <= n:
+        counter += 1
+    print(counter)
+    return triangular_number(counter)
+
+print(first_tri_with_n_factors(500))
--- Python/problem_3.py
+++ Python/problem_3.py
@@ -1,13 +1,7 @@
 import math
 
-def list_constructor(n):
-    int_list = []
-    for i in range(n):
-        int_list.append(i)      #List is initialised with all values set to themselves
-    return int_list
-
 def primes(n):
-    prime_list = list_constructor(n)
+    prime_list = [i for i in range(n)]
     for i in range(2,n):
         if prime_list[i] != 0:   #Check that i has not been crossed off
             for j in range(2*i,n,i): #Cross off every number i away
@@ -15,21 +9,12 @@ def primes(n):
     prime_list[1] = 0
     return [prime for prime in prime_list if prime != 0]  #return list of uncrossed numbers
 
-
-def prime_factorise(big_num):
-    factors = []
-    for prime in primes(big_num // 2):  # cannot have a prime_factor larger than half of it, unless it is
-        if big_num % prime == 0:
-            factors.append(prime)
-    return factors
-
 def is_prime(n):
     # Can check if it is in the list of primes less than or equal to itself but this is inefficient
     for prime in primes((round(math.sqrt(n)))+1):
         if n % prime == 0:
             return False
-    else:
-        return True
+    return True
 
 
 def largest_prime_factor(big_num):
