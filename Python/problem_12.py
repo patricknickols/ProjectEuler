c2ebdaba2cce57e3451a65566b7a3d467d64f2ef
patricknickols <patricknickols@gmail.com>
2021-07-19 11:57:32 +0100

    Solved 11 and tidied 12

--- Python/problem_12.py
+++ Python/problem_12.py
@@ -9,9 +9,6 @@ def primes(n):
     prime_list[1] = 0
     return [prime for prime in prime_list if prime != 0]  #return list of uncrossed numbers
 
-def triangular_number(n):
-    return int((n*(n+1))/2)
-
 def prime_factors(big_num):
     factors = []
     for prime in primes(big_num // 2):  # cannot have a prime_factor larger than half of it, unless it is
@@ -30,6 +27,29 @@ def prime_factors_with_powers(n):
         primes_and_powers[factor] = counter
     return primes_and_powers
 
+def triangular_number(n):
+    return int((n*(n+1))/2)
+
+def multiply_factor_dicts(x,y):
+    result = {}
+    for factor in x:
+        if factor in y:
+            result[factor] = x[factor] + y[factor]
+        else:
+            result[factor] = x[factor]
+    for factor in y:
+        if factor not in result:
+            result[factor] = y[factor]
+    return result
+
+def count_factors_tri_number(n):
+    first_factors = prime_factors_with_powers(n)
+    second_factors = prime_factors_with_powers(n+1)
+    result_factors = multiply_factor_dicts(first_factors, second_factors)
+    result_factors[2] -= 1
+    incr_exponents = [result_factors[key] + 1 for key in result_factors]
+    return product(incr_exponents)
+
 def product(list):
     result = 1
     for element in list:
@@ -42,10 +62,9 @@ def number_of_factors(n):
     return product(incr_exponents)
 
 def first_tri_with_n_factors(n):
-    counter = 1200
-    while number_of_factors(triangular_number(counter)) <= n:
+    counter = 3
+    while count_factors_tri_number(counter) <= n:
         counter += 1
-    print(counter)
     return triangular_number(counter)
 
 print(first_tri_with_n_factors(500))
